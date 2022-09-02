7979376d4811067d1aad89c23335d38069bad25e
William F <santifon15@gmail.com>
2022-09-02 11:29:24 -0500

    add connection to db and ORM models

---
+++ db_models.py
@@ -0,0 +1,22 @@
+from decimal import Decimal
+from peewee import *
+
+db= MySQLDatabase(database= "bsale_test", user= "bsale_test", password= "bsale_test", host= "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com")
+
+class Category(Model):
+    id: int = IntegerField()
+    name: str = CharField()
+
+    class Meta:
+        database= db
+
+class Product(Model):
+    id: int = IntegerField()
+    name: str= CharField()
+    url_image: str= CharField()
+    price: Decimal= DecimalField()
+    discount: int= IntegerField()
+    category: Category= ForeignKeyField(Category, column_name= "category")
+
+    class Meta:
+        database= db
