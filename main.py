515d0642bd70bce80c9a03c7e080f7f5b31f36c1
William F <santifon15@gmail.com>
2022-09-02 11:32:23 -0500

    add query filters

--- main.py
+++ main.py
@@ -10,3 +10,17 @@ async def root(q: str | None= None, category: str | None = None, order: str | No
     """ Get all products store in the database. Params: - query: filter products by 'query'. - category: filter products by categor - order: order products ascending or descending. - page: pagination for products. returns: List of products: """
     db.connect()
     query= db_product.select(db_product, db_category).join(db_category).paginate(1, ITEMS_PER_PAGE)
+    if q:
+        query= query.where(db_product.id.in_(db_product.select(db_product.id).join(db_category).where(db_product.name.contains(q))))
+    if category:
+        query= query.where(db_product.id.in_(db_product.select(db_product.id).join(db_category).where(db_category.name.contains(category))))
+    if order:
+        if order == "DESC":
+            query= query.order_by(db_product.name.desc())
+        else:
+            query= query.order_by(db_product.name)
+    if page:
+        query= query.paginate(page, ITEMS_PER_PAGE)
+    products: list[Product]= [Product(id= p.id, name= p.name, url_image= p.url_image, price= p.price, discount= p.discount, category= Category(id= p.category.id, name= p.category.name)) for p in query]
+    db.close()
+    return products
